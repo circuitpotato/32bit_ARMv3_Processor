b3d19334f0f5f192dd6b91d972b4f222d932d069
circuitpotato <135819236+circuitpotato@users.noreply.github.com>
2023-11-04 13:12:11 +0800

    Update ARM_sample.s

--- lab4/ARM_sample.s
+++ lab4/ARM_sample.s
@@ -20,168 +20,139 @@
 	
 ; ------- <code memory (ROM mapped to Instruction Memory) begins>
 ; Total number of instructions should not exceed 127 (126 excluding the last line 'halt B halt').
-
-
-		;R9,R10,R11 to hold values for DP operations
-		LDR R9, Calc_val1		;R9 = 0x0000000A
-		LDR R10, Calc_val2		;R10 = 0x00000008
-		LDR R11, Calc_val3		;R11 = 0x80000000
+;Loop Main Branch Until SW0 on -> Use recorded switch value for operation	
+		;REGISTERS FOR SWITCH CHECK AND 16 DP INSTRUCTION LOADED
+		LDR R10, Calc_val1		;R10 = 0x0000000A
+		LDR R11, Calc_val2		;R11 = 0x00000008
 		LDR R7, ZERO 			;R7 = 0x00000000
-		
-		LDR R5, DIPS			;R0 = 0x00000C04
-main		
-		LDR R2, [R5]			;Check DIPS STATUS if SW1 is flipped?
+		LDR R8, ONE				;R8 = 0x00000001 
+		LDR R9, THREE			;R9 = 0x00000003
+		LDR R4, SEVENSEG		;R4 = 0x00000C18
+		LDR R5, DIPS			;R5 = 0x00000C04
+
+MAIN
+		STR R7, [R4]				;Fill 7Seg with 0's -> Configure switch for operation then flip SW0
+		LDR R2, [R5]				;Check DIPS STATUS for Instruction -> initial is 0x00000000
+		;AND R3, R2, R8				;It checks SW0 on
 		CMP R2, R7
-		BEQ main
+		BNE BRANCHPREDICT
 		
-;This branch showcases our processor's ability to: LDR/STR with Positive & Negative offset, DP Src2 imm operation 
-;Basic_operations
-		;LDR
-		LDR R6, ZERO			;R6 = 0x00000000	;R6 init to 0
-		LDR R8, Delay			;R8 = 0x00000005	;Reset Delay value when Looped back into this branch
-		LDR R1, LEDS			;R1 = 0x00000C00
-		;DP Src2 imm operation
-		ADD R1, #0x1C			;R1 = 0x00000C1C
-		;STR with Negative offset
-		STR R9, [R1, #-4]		;7Seg = 0x0000000A
-		;DP Src2 imm operation
-		SUB R1, #8				;R1 = 0x00000C14
-		;STR with Positive offset
-		STR R10, [R1, #4]		;7Seg = 0x00000008	;looking at 0xC18 address which is 7seg addr
+		;AND R3, R2, R9				;It checks SW0 and SW1 on
+		;CMP R3, R9			
+		;BEQ BRANCHPREDICT
 		
-;DP_operations
-		;Setting Up Registers for DP operations
-		LDR R1, SEVENSEG		;R1 = 0x00000C18
-		
-		;DP Src2 Reg
-		SUB R6, R9, R10
-		STR R6, [R1]			;7seg = 0x00000002
-		
-		;DP Src2 Reg + imm shifts
-		;LSL
-		ADD R6, R9, R10, LSL #2
-		STR R6, [R1] 			;7seg = 0x0000002A
-		;LSR
-		ADD R6, R9, R10, LSR #2
-		STR R6, [R1]			;7seg = 0x0000000C
-		;ASR
-		ADD R6, R9, R11, ASR #2
-		STR R6, [R1]			;7seg = 0xE000000A
-		;ROR
-		ADD R6, R9, R10, ROR #4	
-		STR R6, [R1]			;7seg = 0x8000000A
-		
-		;AND 
-		AND R6, R9, R10
-		STR R6, [R1]			;7seg = 0x00000008
-		
-		;ORR
-		ORR R6, R9, R10
-		STR R6, [R1]			;7seg = 0x0000000A
+		;AND
+		AND R1, R10, R11			;0x0000000A AND 0x00000008
+		STR R1, [R4]				;7seg = 0x00000008
 		
 		;EOR
-		EOR R6, R9, R10			;7seg = 0x00000002
-		STR R6, [R1]
+		EOR R1, R10, R11			;0x0000000A EOR 0x00000008
+		STR R1, [R4]				;7seg = 0x00000002
 		
-		;RSB 
-		RSB R6, R9, R10			;7seg = 0xFFFFFFFE
-		STR R6, [R1]
-	
+		;SUB
+		SUB R1, R10, R9				;0x0000000A SUB 0x00000003
+		STR R1, [R4]				;7seg = 0x00000007
 		
-		;ADC 
-		ADC R6, R9, R10			;7seg = 0x00000013
-		STR R6, [R1]
+		;RSB
+		RSB R1, R10, R11			;0x0000000A RSB 0x00000008
+		STR R1, [R4]				;7seg = 0xFFFFFFFE
+
+		;ADD
+		ADD R1, R10, R11			;0x0000000A ADD 0x00000008
+		STR R1, [R4]				;7seg = 0x00000012
+		
+		;ADC
+		LDR R2, End_7seg			;R2 = 0xFFFFFFFF
+		ADDS R1, R2, R8				;Raise Carry Flag
+		ADC R1, R10, R11			;0x0000000A ADD 0x00000008 + 0x00000001
+		STR R1, [R4]				;7seg = 0x00000013
 		
 		;SBC
-		SBC R6, R9, R10			;7seg = 0x00000002
-		STR R6, [R1]
+		ADDS R1, R2, R7				;reset carry = 0
+		SBC R1, R10, R11			;0x0000000A SUB 0x00000008 - 0x00000001 i
+		STR R1, [R4]				;7seg = 0x00000001
 		
-		;MOV
-		MOV R6, R9		        ;7seg = 0x0000000A
-		STR R6, [R1]
-
-		;MVN
-		MVN R6, R9				;7seg = 0xFFFFFFF5
-		STR R6, [R1]
-
-		;BIC
-		BIC R6, R6, #0x1F 		;7seg = 0xFFFFFFE0
-		STR R6, [R1]
-		
-		;RSC 
-		RSC R6, R9, R10			;7seg = 0xFFFFFFFE
-		STR R6, [R1]
+		;RSC
+		ADDS R1, R2, R7				;reset carry = 0 
+		RSC R1, R10, R11			;0x00000008 SUB 0x0000000A - 0x00000001 (not Carry Flag = not 0 = 1)
+		STR R1, [R4]				;7seg = 0xFFFFFFFD
 		
 		;TST
-		TST R9, R7 
-		BEQ TST_flagup
-TST_flagup
-		LDR R6, TST_flag
-		STR R6, [R1]			;7seg = 0xDEDAAAAA
+		TST R2, R7					;0xFFFFFFFF AND 0x00000000	= 0x00000000
+		BEQ TST_FLAGUP
 
+TST_FLAGUP
+		STR R4, [R4]				;7seg = 0x00000C18	JUST TO SHOW IT BRANCHED
+		
 		;TEQ
-		TEQ R9, R9 
-		BEQ TEQ_flagup
-TEQ_flagup
-		LDR R6, TEQ_flag
-		STR R6, [R1]			;7seg = 0xDADAAAAA
+		TEQ R2, R2					;0xFFFFFFFF XOR 0xFFFFFFFF	= 0x00000000
+		BEQ TEQ_FLAGUP
+
+TEQ_FLAGUP
+		STR R5, [R4]				;7seg = 0x00000C04	JUST TO SHOW IT BRANCHED
 		
-;CMP + CMN Operations
-;Cmp_op1	;Using Z flag
-		CMP R9, R9
-		BEQ Cmp_flagup
-		B Exit					; exit condition in case it fails --> just for completeness
-Cmp_flagup
-		LDR R6, Cmp_flag
-		STR R6, [R1]			;7seg = 0xAAAAAAAA
-
-
-;Cmp_op2	;Using Z flag
-		CMP R9, R10
-		BNE Cmp_flagdown		
-		B Exit
-Cmp_flagdown
-		LDR R6, Cmp_noflag
-		STR R6, [R1] 			;7seg = 0xBBBBBBBB
-
+		;CMP
+		CMP R2, R2					;0xFFFFFFFF SUB 0xFFFFFFFF	= 0x00000000
+		BEQ CMP_FLAGUP
 		
-;Cmn_op1	;Using N flag
-		CMN R9, R11
-		BMI Cmn_flagup
-		B Exit
-Cmn_flagup
-		LDR R6, Cmn_flag
-		STR R6, [R1] 			;7seg = 0xCCCCCCCC
-	
+CMP_FLAGUP
+		STR R4, [R4]				;7seg = 0x00000C18	JUST TO SHOW IT BRANCHED
+
+		;CMN
+		CMN R2, R7					;C FLAG IS NOT RAISE
+		BCC CMN_FLAGUP
+
+CMN_FLAGUP
+		STR R5, [R4]				;7seg = 0x00000C04	JUST TO SHOW IT BRANCHED
 		
-;Cmn_op2 ;Using N flag
-		CMN R9, R10
-		BPL Cmn_flagdown
-		B Exit
-Cmn_flagdown
-		LDR R6, Cmn_noflag
-		STR R6, [R1] 			;7seg = 0xDDDDDDDD
-		B End_loop
-
-;Delay loop to serve as countdown before restarting from top
-End_loop
-		STR R8, [R1]			;7seg = 0x0000000[5,4,3,2,1]
-		;SUBS not uses, Combination of CMP + SUB to carry out SUBS function
-		CMP R8, #1
-		SUB R8, #1
-		BNE End_loop
+		;ORR
+		ORR R1, R10, R11			;0x0000000A ORR 0x00000008
+		STR R1, [R4]				;7seg = 0x0000000A
 		
-		B End_display
+		;MOV
+		MOV R1, R8					;MOVE 0X00000001 TO R1
+		STR R1, [R4]				;7seg = 0x00000001
+		
+		;BIC
+		BIC R1, R2, R8				;BIT CLEAR 1 FIRST BIT OF 0xFFFFFFFF
+		STR R1, [R4]				;7seg = 0xFFFFFFFE
+		
+		;MVN
+		MVN R1, R10					;NOT A MOVE TO R1
+		STR R1, [R4]				;7seg = 0xFFFFFFF5
+		
+		B MAIN
+		
+;Reset 
+		;LDR R1, RST					
+		;STR R1, [R7]				;7seg = 0x0123ABCD
+		;LDR R2, [R5]				;Check DIPS for SW0 to turn off before resetting to Main
+		;AND R3, R2, R8				;No LSR needed for this status check because It checks SW0 on
+		;CMP R3, R7					;Check SW0 Status
+		;BNE Reset
+		;BEQ MAIN
+		
+;BRANCH PREDICTION INSTRUCTIONS NOT MEANT FOR PHYSICAL - JUST WANT THE INSTRUCTION FOR HDL SIM
+BRANCHPREDICT
+		;LOAD REGISTERS FOR BRANCH PREDICTION INSTRUCTIONS
+		LDR R1, ZERO
+		LDR R2, ONE
+		LDR R3, FIVE
+		LDR R4, NINE
+		
+BRANCH5X
+		;ADD R1, R1, R2
+		SUBS R3, R3, R2
+		BNE BRANCH5X				;BRANCH TO BRANCH5X 5 TIMES -BNE CHECK Z FLAG
+		
+		B SELECTBRANCH
 
-;If CMP or CMN does not meet desired flag conditions, which is not meant to, done for completeness	
-Exit
-		LDR R6, Exit_7seg
-		STR R6, [R1]			;7seg = 0xEEEEEEEE
-		B halt
+BRANCH10X
+		SUBS R4, R4, R2				;CAUSES LOOP 10X TO THIS BRANCH
 
-End_display 
-		LDR R6, End_7seg
-		STR R6, [R1]			;7seg = 0xFFFFFFFF
+SELECTBRANCH
+		BMI BRANCHPREDICT			;BRANCH BACK TO START WHEN NEGATIVE FLAG SET MEANS LOOPED 10X COMPLETE
+		BPL BRANCH10X				;BRANCH BACK TO BRACH10X TO CONTINUE THE LOOP - N FLAG NOT RAISED
 
 halt	
 		B    halt           ; infinite loop to halt computation. // A program should not "terminate" without an operating system to return control to
@@ -210,34 +181,25 @@ SEVENSEG
 
 
 ; Rest of the constants should be declared below.
+ONE
+		DCD 0x00000001		; Bit Mask/Other uses
+THREE
+		DCD 0x00000003		; Bit Mask -> Last 4 Bits = 0011
 ZERO
 		DCD 0x00000000		; constant 0
-ONE
-		DCD 0x00000001		; constant 0
+FIVE
+		DCD 0x00000005		;CONSTANT 5
+NINE
+		DCD 0X00000009		;CONSTANT 9
 Calc_val1
-		DCD 0x0000000A		;Value 1 for DP operations
+		DCD 0x0000000A
+
 Calc_val2
-		DCD 0x00000008		;Value 2 for DP operations
-Calc_val3
-		DCD 0x80000000
-TST_flag
-		DCD 0xDEDAAAAA		;Indicate that TST Z flag raised
-TEQ_flag
-		DCD 0xDADAAAAA		;Indicate that TST Z flag raised
-Cmp_flag
-		DCD 0xAAAAAAAA		;Indicate that CMP Z flag raised
-Cmp_noflag
-		DCD 0xBBBBBBBB		;Indicate that CMP Z flag not raised
-Cmn_flag
-		DCD 0xCCCCCCCC		;Indicate that CMN V flag raised
-Cmn_noflag
-		DCD 0xDDDDDDDD		;Indicate that CMN V flag not raised
-Delay
-		DCD 0x00000005		;5 step delay
-Exit_7seg
-		DCD 0xEEEEEEEE
+		DCD 0x00000008		
+RST
+		DCD 0x0123ABCD
 End_7seg
-		DCD 0XFFFFFfFF
+		DCD 0xFFFFFFFF
 		
 ; ------- <constant memory (ROM mapped to Data Memory) ends>	
 

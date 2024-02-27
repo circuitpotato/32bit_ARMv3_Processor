bec900793fa5196510d73b256903ab4ea97e6236
circuitpotato <135819236+circuitpotato@users.noreply.github.com>
2024-02-27 15:08:58 +0800

    Update ALU.vhd

--- lab4/Normal_PipelineWithHazard/ALU.vhd
+++ lab4/Normal_PipelineWithHazard/ALU.vhd
@@ -17,18 +17,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-----------------------------------------------------------------------------------
---	License terms :
---	You are free to use this code as long as you
---		(i) DO NOT post it on any public repository;
---		(ii) use it only for educational purposes;
---		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of ARM Holdings or other entities.
---		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
---		(v)	acknowledge that the program was written based on the microarchitecture described in the book Digital Design and Computer Architecture, ARM Edition by Harris and Harris;
---		(vi) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course CG3207 at the National University of Singapore);
---		(vii) retain this notice in this file or any files derived from this.
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
